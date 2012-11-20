844e780a6151063169195da5f828556e9900829f
Carlos Martín <cmartin@liberalia.net>
2012-11-20 18:36:58 +0100

    Remove unused bottle stuff

--- setup.py
+++ setup.py
@@ -1,34 +1,27 @@
 #!/usr/bin/env python
 
 import sys
-import os
-from distutils.core import setup
+from setuptools import setup
 
 if sys.version_info < (2,5):
-    raise NotImplementedError("Sorry, you need at least Python 2.5 or Python 3.x to use bottle.")
+    raise NotImplementedError("Sorry, you need at least Python 2.5 or Python 3.x to use routes.")
 
-import bottle
+import routes
 
-setup(name='bottle',
-      version=bottle.__version__,
+setup(name='routes',
+      version=routes.__version__,
       description='Fast and simple WSGI-framework for small web-applications.',
-      long_description=bottle.__doc__,
-      author=bottle.__author__,
+      long_description=routes.__doc__,
+      author=routes.__author__,
       author_email='marc@gsites.de',
-      url='http://bottlepy.org/',
-      py_modules=['bottle'],
-      scripts=['bottle.py'],
+      py_modules=['routes'],
       license='MIT',
       platforms = 'any',
+      test_suite = 'test',
       classifiers=['Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
         'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
-        'Topic :: Internet :: WWW/HTTP :: WSGI',
-        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
-        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
-        'Topic :: Internet :: WWW/HTTP :: WSGI :: Server',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
